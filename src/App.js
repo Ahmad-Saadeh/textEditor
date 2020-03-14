af2a10f5e5e4e2534548b36a4f247abae5f0d54a
Ahmad-Saadeh <Ahmad-Saadeh@users.noreply.github.com>
2020-03-14 21:26:26 +0200

    textEditor

--- src/App.js
+++ src/App.js
@@ -12,9 +12,29 @@ const stylings = ["bold", "italic", "underline"];
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 class App extends Component {
+  state = {
+    underline: false,
+    bold: false,
+    italic: false,
+    currentColor: null
+  };
+
+  fontStyle = style => {
+    this.setState({ [style]: !this.state[style] });
+  };
+
+  fontColor = color => {
+    this.setState({ currentColor: color });
+  };
+
   render() {
     const stylingBoxes = stylings.map(style => (
-      <button className="btn btn-light" style={styles[style]} key={style}>
+      <button
+        className="btn btn-light"
+        style={styles[style]}
+        key={style}
+        onClick={() => this.fontStyle(style)}
+      >
         {style}
       </button>
     ));
@@ -23,13 +43,21 @@ class App extends Component {
       <button
         style={{ backgroundColor: color, height: 30, width: 30 }}
         key={color}
+        onClick={() => this.fontColor(color)}
       />
     ));
 
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            fontStyle: this.state.italic ? "italic" : "",
+            fontWeight: this.state.bold ? "bold" : "",
+            textDecorationLine: this.state.underline ? "underline" : "",
+            color: this.state.currentColor
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
